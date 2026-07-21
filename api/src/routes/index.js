c78165052f1ab7a2c10a5de282289e1ccd3b1aa8
hoanganh732 <85354504+hoanganh732@users.noreply.github.com>
2026-07-21 10:19:46 +0700

    thêm route weather3

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,19 +3,17 @@ const weatherController = require("../controllers/weather.controller.js");
 
 module.exports = (app) => {
   app.get("/", (req, res) => {
-    res.json({ message: "🚀 Server is running!" });
+    res.json({ message: "🚀 Server is running! ĐÃ CÓ THỜI TIẾT V2" });
   });
 
   app.use("/locket", locketRouter);
-
 
   // anhdev yêu cầu thêm endpoint này để front-end có thể lấy được theme mặc định (trước đây là null nên bị lỗi khi gọi .filter())
   app.get("/v1/public/themes", (req, res) => {
     res.json([]); // Trả về mảng rỗng để hàm .filter() ở front-end không bị lỗi null
   });
 
-  app.post("/api/weatherV2", weatherController.getWeather);// Thêm route cho API thời tiết
+  app.post("/api/weatherV2", weatherController.getWeather); // Thêm route cho API thời tiết
 
   app.use("/locket", locketRouter);
 };
-
